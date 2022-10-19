7098d9dbfdd7a9383717d00570b45083a8a78c6a
THAINA OLIVEIRA ARAUJO <thaina.oaraujo1@senacsp.edu.br>
2022-10-19 09:13:19 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express,{Request, Response} from 'express'
+import mainRoutes from './routes/index'
+import path from 'path'
+//importando o mustache para usar o HTML
+import mustache from 'mustache-express'
+// importando as variaveis de ambiente
+import dotenv from 'dotenv'
+
+//configurando as variaveis
+dotenv.config()
+
+//USANDO EXPRESS
+const server = express()
+//configurando o mustache
+server.set('view engine','mustache')
+//criando uma rota para a pasta public
+server.use(express.static(path.join(__dirname,'../public')))
+//criando a rota para a pasta views
+server.set('views',path.join(__dirname,'views'))
+server.engine('mustache',mustache())
+
+//HABILITANDO O POST 
+server.use(express.urlencoded({extended:true}))
+
+server.use(mainRoutes)
+//criando a página não encontrada
+server.use( (req:Request, res:Response) =>{
+    res.status(404).send("Página não encontrada")
+})
+server.listen(process.env.port)
